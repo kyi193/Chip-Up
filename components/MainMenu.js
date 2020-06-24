3a0d4230e29798417f5bb0549b16fddcfa7c17a1
Kevin <kevin.yi93@gmail.com>
2020-06-23 20:30:47 -0400

    Add circular action menu in <MainMenu>

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import ActionButton from '../assets/circularTools/ActionButton.js'
+import Icon from 'react-native-vector-icons/Ionicons';
+
 class MainMenu extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is the main menu</Text>
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+            <Icon name="android-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => { }}>
+            <Icon name="android-notifications-none" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => { }}>
+            <Icon name="android-done-all" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
       </View>
     )
   }
@@ -15,6 +28,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  }
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
 })
 export default MainMenu
