affc83c61169737a446867795dc863c7cb821647
Kevin <kevin.yi93@gmail.com>
2020-06-24 21:59:28 -0400

    Set default # of players to 9 and highlight selected box to orange

--- components/ChipUpPlayerNum.js
+++ components/ChipUpPlayerNum.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 const numOfPlayers = [9, 8, 7, 6, 5, 4, 3, 2]
 class ChipUpPlayerNum extends Component {
+  state = {
+    playerNum: 9
+  }
+
+  playerNumSelection = key => (event) => {
+    this.setState(() => ({
+      playerNum: key
+    }))
+  }
   render() {
+    const { playerNum } = this.state
     return (
       <View>
         <Text>How many players?</Text>
         <View style={styles.numOfPlayers}>
           {numOfPlayers.map((number) => (
-            <TouchableOpacity style={styles.playerNumBox}>
-              <Text style={styles.playerNum}>{number}</Text>
-            </TouchableOpacity>
+            number === playerNum
+              ? <TouchableOpacity
+                key={number}
+                style={styles.playerNumBoxSelected}
+                onPress={this.playerNumSelection(number)}>
+                <Text style={styles.playerNum}>{number}</Text>
+              </TouchableOpacity>
+              : <TouchableOpacity
+                key={number}
+                style={styles.playerNumBox}
+                onPress={this.playerNumSelection(number)}>
+                <Text style={styles.playerNum}>{number}</Text>
+              </TouchableOpacity>
           ))}
         </View>
       </View>
@@ -30,6 +50,14 @@ const styles = StyleSheet.create({
     borderColor: 'orange',
     borderWidth: 2,
     marginLeft: .5,
+  },
+  playerNumBoxSelected: {
+    height: 45,
+    width: 45,
+    borderColor: 'orange',
+    borderWidth: 2,
+    marginLeft: .5,
+    backgroundColor: 'orange'
   },
   playerNum: {
     marginTop: 7,
