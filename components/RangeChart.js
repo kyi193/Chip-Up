05a4f0791e90f20e91bdc7566ef48987b35f77a3
Kevin <kevin.yi93@gmail.com>
2020-06-27 18:28:22 -0400

    Change range chart box dimensions based on platform used

--- components/RangeChart.js
+++ components/RangeChart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Platform } from 'react-native'
 import { handChart, ranges } from '../utils/helpers'
 import { connect } from 'react-redux'
 
@@ -12,10 +12,14 @@ class RangeChart extends Component {
         {handChart.map((row) => (
           row.map((hand) => (
             range.includes(hand)
-              ? <View style={styles.highlighted} key={hand}>
+              ? <View style={(Platform.OS === 'ios' || Platform.OS === 'android')
+                ? styles.highlightedios
+                : styles.highlightedweb} key={hand}>
                 <Text style={{ color: 'white', fontSize: 12, padding: 2, textAlign: 'center' }}>{hand}</Text>
               </View>
-              : <View style={styles.unhighlighted} key={hand}>
+              : <View style={(Platform.OS === 'ios' || Platform.OS === 'android')
+                ? styles.unhighlightedios
+                : styles.unhighlightedweb} key={hand}>
                 <Text style={{ color: 'white', fontSize: 12, padding: 2, textAlign: 'center' }}>{hand}</Text>
               </View>
           ))
@@ -32,20 +36,34 @@ const styles = StyleSheet.create({
     width: '100%',
     flexWrap: 'wrap'
   },
-  highlighted: {
+  highlightedios: {
+    height: 28.5,
+    width: 28.5,
+    backgroundColor: 'green',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  unhighlightedios: {
+    height: 28.5,
+    width: 28.5,
+    backgroundColor: 'gray',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  highlightedweb: {
     height: 50,
     width: 50,
     backgroundColor: 'green',
     borderColor: 'black',
     borderWidth: 1,
   },
-  unhighlighted: {
+  unhighlightedweb: {
     height: 50,
     width: 50,
     backgroundColor: 'gray',
     borderColor: 'black',
     borderWidth: 1,
-  }
+  },
 })
 
 function mapStateToProps(state) {
--- utils/helpers.js
+++ utils/helpers.js
@@ -96,5 +96,24 @@ export const handChart = [
 ]
 
 export const ranges = {
-  utg200: ['A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'AKo']
+  utg200: ['A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'AKo'],
+  utg190: ['A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'AKo', 'TT'],
+  utg180: ['A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'AJs', 'AKo', 'TT'],
+  utg170: ['A3s', 'A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'AJs', 'AKo', 'TT'],
+  utg160: ['A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'AJs', 'AKo', 'TT'],
+  utg150: ['A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'AJs', 'AKo', 'TT'],
+  utg140: ['A3s', 'A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'AJs', 'ATs', 'AKo', 'AQo', 'TT'],
+  utg130: ['A3s', 'A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'AJs', 'ATs', 'AKo', 'AQo', 'TT', '99'],
+  utg120: ['A3s', 'A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'AJs', 'ATs', 'AKo', 'AQo', 'TT', '99'],
+  utg110: ['A3s', 'A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'AKo', 'AQo', 'TT', '99'],
+  utg100: ['A3s', 'A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'KTs', 'QTs', 'AKo', 'AQo', 'TT', '99', '88', '77'],
+  utg90: ['A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'KTs', 'QTs', 'JTs', 'AKo', 'AQo', 'TT', '99', '88', '77'],
+  utg80: ['A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'KTs', 'QTs', 'JTs', 'AKo', 'AQo', 'AJo', 'TT', '99', '88', '77', '66'],
+  utg70: ['A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'A9s', 'KTs', 'QTs', 'JTs', 'AKo', 'AQo', 'AJo', 'KQo', 'TT', '99', '88', '77', '66', '55'],
+  utg60: ['A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'A9s', 'A8s', 'K9s', 'T9s', 'KTs', 'QTs', 'JTs', 'AKo', 'AQo', 'KQo', 'AJo', 'ATo', 'TT', '99', '88', '77', '66', '55'],
+  utg50: ['A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'A9s', 'A8s', 'A7s', 'K9s', 'T9s', 'KTs', 'QTs', 'JTs', 'AKo', 'AQo', 'KQo', 'AJo', 'ATo', 'TT', '99', '88', '77', '66', '55', '44'],
+  utg40: ['A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'A9s', 'A8s', 'A7s', 'A6s', 'K9s', 'T9s', 'KTs', 'QTs', 'JTs', 'AKo', 'AQo', 'KQo', 'KJo', 'AJo', 'ATo', 'TT', '99', '88', '77', '66', '55', '44'],
+  utg30: ['A3s', 'A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'A9s', 'A8s', 'A7s', 'A6s', 'K9s', 'Q9s', 'J9s', 'T9s', 'KTs', 'QTs', 'JTs', 'AKo', 'AQo', 'KQo', 'KJo', 'KTo', 'AJo', 'ATo', 'TT', '99', '88', '77', '66', '55', '44'],
+  utg20: ['A2s', 'A3s', 'A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'A9s', 'A8s', 'A7s', 'A6s', 'K9s', 'Q9s', 'J9s', 'T9s', 'KTs', 'QTs', 'JTs', 'AKo', 'AQo', 'KQo', 'KJo', 'KTo', 'AJo', 'ATo', 'TT', '99', '88', '77', '66', '55', '44', 'A9o', 'A8o', 'A7o', 'A6o', 'A5o', 'A4o', 'A3o', 'K9o', 'QJo', 'QTo', 'JTo', 'K8s', 'Q8s', 'J8s', 'T8s', 'K7s', 'K6s', 'K5s', 'K4s', '33', '22', '98s'],
+  utg10: ['A2s', 'A3s', 'A4s', 'A5s', 'AA', 'KK', 'QQ', 'JJ', 'AKs', 'AQs', 'KQs', 'KJs', 'QJs', 'AJs', 'ATs', 'A9s', 'A8s', 'A7s', 'A6s', 'K9s', 'Q9s', 'J9s', 'T9s', 'KTs', 'QTs', 'JTs', 'AKo', 'AQo', 'KQo', 'KJo', 'KTo', 'AJo', 'ATo', 'TT', '99', '88', '77', '66', '55', '44', 'A9o', 'A8o', 'A7o', 'A6o', 'A5o', 'A4o', 'A3o', 'A2o', 'K9o', 'K8o', 'K7o', 'K6o', 'K5o', 'K4o', 'K3o', 'K2o', 'QJo', 'QTo', 'Q9o', 'Q8o', 'Q7o', 'Q6o', 'Q5o', 'Q4o', 'JTo', 'J9o', 'J8o', 'J7o', 'T9o', 'T8o', 'T7o', '98o', '97o', '87o', '86o', '76o', 'K8s', 'Q8s', 'J8s', 'T8s', 'K7s', 'K6s', 'K5s', 'K4s', 'K3s', 'K2s', '33', '22', 'Q7s', 'Q6s', 'Q5s', 'Q4s', 'Q3s', 'Q2s', 'J7s', 'J6s', 'J5s', 'J4s', 'J3s', 'J2s', 'T7s', 'T6s', 'T5s', 'T4s', 'T3s', 'T2s', '98s', '97s', '96s', '95s', '94s', '93s', '87s', '86s', '85s', '84s', '76s', '75s', '74s', '65s', '64s', '63s', '54s', '53s', '43s'],
 }
