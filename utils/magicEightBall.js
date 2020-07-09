6ea42323cad9d1e67669b8f23b096bbdabd0ea72
Kevin <kevin.yi93@gmail.com>
2020-07-09 18:37:36 -0400

    Create OnThisMonthResponse class

--- utils/magicEightBall.js
+++ utils/magicEightBall.js
@@ -76,3 +76,46 @@ export class BetterNotTellResponse {
     }
   }
 }
+
+export class OnThisMonthResponse {
+  constructor() {
+    this.months = ['January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December']
+    this.when = ['before', 'after']
+    this.randomMonth
+    this.randomWhen
+  }
+  getAnswer() {
+    const randomMonthIdx = Math.floor(Math.random() * this.months.length)
+    const randomWhenIdx = Math.floor(Math.random() * this.when.length)
+    const randomMonth = this.months[randomMonthIdx]
+    const randomWhen = this.when[randomWhenIdx]
+    this.randomMonth = randomMonth
+    this.randomWhen = randomWhen
+    if (randomMonth === 'January' && randomWhen === 'before') {
+      randomWhen = 'after'
+    }
+    if (randomMonth === 'December' && randomWhen === 'after') {
+      randomWhen = 'before'
+    }
+    return `If you were born ${randomWhen} the month of ${randomMonth}, then yes`
+  }
+  getResults() {
+    return {
+      answer: this.getAnswer(),
+      index: 6,
+      randomWhen: this.randomWhen,
+      randomMonth: this.randomMonth
+    }
+  }
+}
