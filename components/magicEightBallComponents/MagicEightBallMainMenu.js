f4d199ff514b4c9cca0364876ed1dcb777962eae
Kevin <kevin.yi93@gmail.com>
2020-07-10 00:50:12 -0400

    Center the view of Magic Eight Ball

--- components/magicEightBallComponents/MagicEightBallMainMenu.js
+++ components/magicEightBallComponents/MagicEightBallMainMenu.js
@@ -3,11 +3,18 @@ import { View, Text, StyleSheet, Platform } from 'react-native'
 class MagicEightBallMainMenu extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>This is the Magic Eight Ball</Text>
       </View>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 export default MagicEightBallMainMenu
