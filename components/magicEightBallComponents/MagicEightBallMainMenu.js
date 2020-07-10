af30a0c9bba9de0b79f669fcf2f05e03a2b9672b
Kevin <kevin.yi93@gmail.com>
2020-07-10 01:11:52 -0400

    Create text input and submit button that clears input field

--- components/magicEightBallComponents/MagicEightBallMainMenu.js
+++ components/magicEightBallComponents/MagicEightBallMainMenu.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Platform } from 'react-native'
+import { View, Text, StyleSheet, Platform, TextInput, TouchableWithoutFeedback, Keyboard, TouchableOpacity } from 'react-native'
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity
+      style={{ justifyContent: 'center', alignItems: 'center', height: 50, width: 75, borderWidth: 1, borderColor: 'black' }}
+      onPress={onPress}>
+      <Text>Ask</Text>
+    </TouchableOpacity>
+  )
+}
 class MagicEightBallMainMenu extends Component {
+  state = {
+    textInput: ''
+  }
+  onChangeText = (text) => {
+    this.setState(() => ({
+      textInput: text
+    }))
+  }
+
+  submitQuestion = () => {
+    this.setState(() => ({
+      textInput: ''
+    }))
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is the Magic Eight Ball</Text>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.container}>
+            <Text>Please ask a yes or no question</Text>
+            <TextInput
+              style={styles.textInput}
+              onChangeText={text => this.onChangeText(text)}
+              value={this.state.textInput}
+            />
+            <SubmitBtn onPress={this.submitQuestion} />
+          </View>
+        </TouchableWithoutFeedback>
       </View>
     )
   }
@@ -15,6 +48,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  textInput: {
+    height: 40,
+    width: 350,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 10
   }
 })
 export default MagicEightBallMainMenu
